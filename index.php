e3e460160cd58236b3a20a3001f07b7dd5d06049
Donnabauer Christian <christiandonnabauer@gmail.com>
2019-04-22 11:46:16 +0200

    get all Entries from db into table

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+    $_db_host = "localhost";
+    $_db_datenbank = "stats";
+    $_db_username = "user";
+    $_db_passwort = "user";
+    $conn = new mysqli($_db_host, $_db_username, $_db_passwort, $_db_datenbank);
+
+    if($conn->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Stats</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <h1>create Payment/Income:</h1><br>
+        <form id="entry" action="" method="post" >
+            Name: <input type="text" name="name" id="name" required><br> <!-- necessary with purpose? -->
+            Type: <select name="type" form="entry" id="type" required>
+                <option value="income">Income</option>      <!-- if income/payment is true only purposes for incomes/payments should be displayed-->
+                <option value="payment">Payment</option>
+            </select><br>
+            Purpose: <select name="purpose" form="entry" id="purpose" required>
+                <option value="food">Food</option>
+                <option value="clothes">Clothes</option>
+                <option value="work">Work</option>
+                <option value="other">Other</option>
+            </select><br>
+            Date: <input type="date" name="date" id="date"><br> <!-- zB.: 2019-04-16 -->
+            Value: <input type="text" name="value" id="value" required><br>
+            <input type="submit"  name="submit">
+        </form>
+
+        <h1>get Payments/Incomes: </h1>
+        <a href="index.php?showEntries=true"><button type="button">get Entries</button></a><br>
+    </body>
+</html>
+<?php
+    if(isset($_POST['submit'])){
+        $data = [$_POST['name'],$_POST['type'],$_POST['purpose'],$_POST['date'], $_POST['value']];
+        $entry = implode(',',$data);
+
+        $insertStatement = "INSERT INTO entries (id, name, type, purpose, date, value) VALUES ('', '$data[0]', '$data[1]', '$data[2]', '$data[3]', '$data[4]');";
+        if ($_res = $conn->query($insertStatement)){
+            echo "<br>Entry has been added to the database.";
+        }else {
+            echo "<br>No insertion into database";
+        }
+        $conn->close();
+    }
+
+    if (isset($_GET['showEntries'])) {
+        $selectStatement = "SELECT date, purpose, value FROM ENTRIES";
+        $result = $conn->query($selectStatement);
+
+        if ($result->num_rows > 0) {
+            $out = "<table><tr><th>date:</th><th>purpose:</th><th>value:</th></tr>";
+
+            while($row = $result->fetch_assoc()) {
+                $out = $out."<tr><td>" . $row["date"]. "</td><td>" . $row["purpose"]. "</td><td>" . $row["value"]. "€</td></tr>";
+            }
+
+            $out = $out."</table>";
+            echo $out;
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+    }
+?>
