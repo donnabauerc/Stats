3e18bf0d6c6b6cb01809c4448791975ff2a0f9dc
Christian Donnabauer <donnabauer.christian.01@gmail.com>
2019-05-16 12:45:31 +0200

    Login seperated, redirect is not correct

--- src/php/index.php
+++ src/php/index.php
@@ -1,14 +1,5 @@
 <?php
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-
-    //Connection with DB
-    $_db_host = "localhost";
-    $_db_datenbank = "stats";
-    $_db_username = "user";
-    $_db_passwort = "user";
-    $conn = new mysqli($_db_host, $_db_username, $_db_passwort, $_db_datenbank);
+    $conn = new mysqli($_SESSION['db_host'], $_SESSION['db_username'], $_SESSION['dp_password'], $_SESSION['db_database']);
 
     if($conn->connect_error){
         die("Connection failed: " . $conn->connect_error);
@@ -32,11 +23,6 @@
                     document.getElementById("wrapper").style.backgroundColor = "white";
                 }
             }
-
-            function register(){
-                document.getElementById("loginDiv").style.display = "none";
-                document.getElementById("registerDiv").style.display = "block";
-            }
         </script>
     </head>
     <body>
@@ -50,47 +36,29 @@
                 </ul>
             </div>
             <div id="content">
-                <div id="Div">
-                  <div id="loginDiv">
-                    <h1>LogIn</h1>
-                    <form id="login" method="post" action="">
-                        Username: <input type="text" name="uname" required><br>
-                        Password: <input type="password" name="password" required><br>
-                        <input type="submit" value="LogIn" name="login">
+                <div id="create">
+                    <h1>create Payment/Income:</h1><br>
+                    <form id="entry" action="" method="post" >
+
+                        Name: <input type="text" name="name" id="name" required><br> <!-- necessary with purpose? -->
+                        Type: <select name="type" form="entry" id="type" required>
+                                    <option value="income">Income</option>      <!-- if income/payment is true only purposes for incomes/payments should be displayed-->
+                                    <option value="payment">Payment</option>
+                                </select><br>
+
+                        Purpose: <select name="purpose" form="entry" id="purpose" required>
+                                    <option value="food">Food</option>
+                                    <option value="clothes">Clothes</option>
+                                    <option value="work">Work</option>
+                                    <option value="other">Other</option>
+                                </select><br>
+
+                        Date: <input type="date" name="date" id="date"><br> <!-- zB.: 2019-04-16 -->
+                        Value: <input type="text" name="value" id="value" required><br>
+                        <input type="submit"  name="submit">
                     </form>
-                    <p id="registerSpan" onclick="register()">create Account?</p>
                 </div>
-                <div id="registerDiv">
-                  <h1>Register: </h1>
-                  <form id="register" method="post" action="">
-                    Real Name: <input type="text" name="name" required><br>
-                    Username: <input type="text" name="uname" required><br>
-                    Password: <input type="password" name="password1" required><br>
-                    Password: <input type="password" name="password1" required><br>
-                    <input type="submit" value="Regist" name="register">
-                  </form>
-                </div>
-              </div>
-            <div id="create" style="display: none;">
-                <h1>create Payment/Income:</h1><br>
-                <form id="entry" action="" method="post" >
-                    Name: <input type="text" name="name" id="name" required><br> <!-- necessary with purpose? -->
-                    Type: <select name="type" form="entry" id="type" required>
-                        <option value="income">Income</option>      <!-- if income/payment is true only purposes for incomes/payments should be displayed-->
-                        <option value="payment">Payment</option>
-                    </select><br>
-                    Purpose: <select name="purpose" form="entry" id="purpose" required>
-                        <option value="food">Food</option>
-                        <option value="clothes">Clothes</option>
-                        <option value="work">Work</option>
-                        <option value="other">Other</option>
-                    </select><br>
-                    Date: <input type="date" name="date" id="date"><br> <!-- zB.: 2019-04-16 -->
-                    Value: <input type="text" name="value" id="value" required><br>
-                    <input type="submit"  name="submit">
-                </form>
-            </div>
-                <div id="chartMaxSize" style="width: 400px;height: 400px;position: none;">
+                <div id="chartMaxSize" style="width: 400px;height: 400px;display: none;">
                     <canvas id="myChart" width="400" height="400"></canvas>
                 </div>
 
@@ -101,27 +69,8 @@
 </html>
 
 <?php
-    if(isset($_SESSION['user_id'])){
-        echo "<script>
-                document.getElementById('Div').style.display = 'none';
-                document.getElementById('create').style.display = 'block';
-                document.getElementById('chartMaxSize').style.display = 'block';
-                document.getElementById('printEntries').style.display = 'block';
-            </script>";
-    }else{
-        if(isset($_POST['login'])){
-            $user = [$_POST['uname'], $_POST['password']];
-            $loginStatement = "SELECT * from users WHERE username like ('".$user[0]."') AND password like ('".$user[1]."');";
-            $result = $conn->query($loginStatement);
-
-            if($result->num_rows > 0){
-                echo "<p>Hello, ".$user[0]."</p>";
-                $user = ($result->fetch_assoc());
-                $_SESSION['user_id'] = $user["user_id"];
-            }else{
-                echo "nope";
-            }
-        }
+    if(!isset($_SESSION["user_id"])){
+        Redirect('http://localhost/Stats/src/php/login.php', false);
     }
 
     //new Entry
@@ -132,13 +81,13 @@
         }else{*/
             $data = [$_POST['name'], $_POST['type'], $_POST['purpose'], $_POST['date'], $_POST['value']];
             $entry = implode(',', $data);
-            $insertStatement = "INSERT INTO entries (id, name, type, purpose, date, value, user_id) VALUES ('', '$data[0]', '$data[1]', '$data[2]', '$data[3]', '$data[4]', '".$_SESSION['user_id']."');"; ##muss mit user erweitert werden
+            $insertStatement = "INSERT INTO entries (entry_id, name, type, purpose, date, value, user_id) VALUES ('', '$data[0]', '$data[1]', '$data[2]', '$data[3]', '$data[4]', '".$_SESSION['user_id']."');"; ##muss mit user erweitert werden
             $result = $conn->query($insertStatement);
         //}
     }
 
     //print all Entries
-        $selectStatement = "SELECT date, purpose,type, value, user_id FROM ENTRIES;";
+        $selectStatement = "SELECT date, purpose,type, value FROM ENTRIES WHERE user_id like('".$_SESSION["user_id"]."');";
         $result = $conn->query($selectStatement);
 
         if ($result->num_rows > 0) {
@@ -146,9 +95,9 @@
 
             while($row = $result->fetch_assoc()) {
                 if($row["type"] == "payment"){
-                    $out = $out."<tr bgcolor=\"#FF0000\"><td>". $row["user_id"] ."</td><td>". $row["date"]. "</td><td>" . $row["purpose"]. "</td><td>" . $row["value"]. "€</td></tr>";
+                    $out = $out."<tr bgcolor=\"#FF0000\"><td>". $row["date"]. "</td><td>" . $row["purpose"]. "</td><td>" . $row["value"]. "€</td></tr>";
                 }else{
-                    $out = $out."<tr bgcolor=\"#00FF00\"><td>". $row["user_id"] ."</td><td>". $row["date"]. "</td><td>" . $row["purpose"]. "</td><td>" . $row["value"]. "€</td></tr>";
+                    $out = $out."<tr bgcolor=\"#00FF00\"><td>". $row["date"]. "</td><td>" . $row["purpose"]. "</td><td>" . $row["value"]. "€</td></tr>";
                 }
             }
             $out = $out."</table>";
@@ -190,5 +139,11 @@
                 }
             });
         </script>";
+
+    function Redirect($url, $permanent = false){
+        header('Location: ' . $url, true, $permanent ? 301 : 302);
+        exit();
+    }
+
     $conn->close();
 ?>
---
+++ src/php/login.php
@@ -0,0 +1,105 @@
+<?php
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    $_SESSION['db_host'] = "localhost";
+    $_SESSION['db_database'] = "stats";
+    $_SESSION['db_username'] = "user";
+    $_SESSION['dp_password'] = "user";
+
+    $conn = new mysqli($_SESSION['db_host'], $_SESSION['db_username'], $_SESSION['dp_password'], $_SESSION['db_database']);
+
+    if($conn->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Stats</title>
+
+        <script>
+            function register(){
+                document.getElementById('loginDiv').style.display = 'none';
+                document.getElementById('registerDiv').style.display = 'block';
+            }
+        </script>
+    </head>
+    <body>
+        <div id="auccounts">
+
+            <div id="loginDiv">
+
+                <h1>LogIn</h1>
+
+                <form id="login" method="post" action="">
+                    Username: <input type="text" name="uname" required><br>
+                    Password: <input type="password" name="password" required><br>
+                    <input type="submit" value="LogIn" name="login">
+                </form>
+
+                <p id="registerSpan" style="cursor: pointer;" onclick="register()">create Account?</p>
+            </div>
+
+            <div id="registerDiv" style="display: none;">
+
+                <h1>Register: </h1>
+
+                <form id="register" method="post" action="">
+                    Real Name: <input type="text" name="name" required><br>
+                    Username: <input type="text" name="uname" required><br>
+                    Password: <input type="password" name="password1" required><br>
+                    Password: <input type="password" name="password2" required><br>
+                    <input type="submit" value="Register" name="register">
+                </form>
+
+            </div>
+        </div>
+
+    </body>
+</html>
+<?php
+    if(isset($_SESSION['user_id'])){
+        Redirect('http://localhost/Stats/src/php/index.php', false);
+    }else{
+
+        if(isset($_POST['login'])){
+            $user = [$_POST['uname'], $_POST['password']];
+            $loginStatement = "SELECT * from user  WHERE username like ('".$user[0]."') AND password like ('".$user[1]."');";
+            $result = $conn->query($loginStatement);
+
+            if($result->num_rows > 0){
+                $user = ($result->fetch_assoc());
+                $_SESSION['user_id'] = $user["id"];
+            }else{
+                echo "Sorry, an error has ocurred! ";
+            }
+        }
+
+        if(isset($_POST['register'])){
+            //validation of passwords is still missing
+            $register_user = [$_POST['name'], $_POST['uname'], $_POST['password1'], $_POST['password2']];
+            $user = [$register_user[1], $register_user[2]];
+
+            $registerStatement = "INSERT INTO user (id, username, password, name) VALUES ('', '$register_user[1]', '$register_user[2]', '$register_user[0]');";
+            $result = $conn->query($registerStatement);//überprüfen ob eh noch nicht existiert
+
+            if($result){
+                $user = ($result->fetch_assoc());
+
+                $_SESSION['user_id'] = $user["user_id"];
+            }else{
+                echo 'Sorry, there was an error creating your account';
+            }
+        }
+
+    }
+
+    function Redirect($url, $permanent = false){
+        header('Location: ' . $url, true, $permanent ? 301 : 302);
+        exit();
+    }
+
+    $conn->close();
+?>
